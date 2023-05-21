20fd081f856a7b886710d08235786c635d0d98a8
SherifMega <shriefmarcato@gmail.com>
2023-05-22 01:00:26 +0300

    trigger github action

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Islamic App!</Text>
+      <Text>Islamic App!!</Text>
       <StatusBar style="auto" />
     </View>
   );
