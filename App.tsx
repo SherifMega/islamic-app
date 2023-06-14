37f038d70ebd37939ce91b7cee7ef16d047879a3
SherifMega <shriefmarcato@gmail.com>
2023-06-14 18:24:54 +0300

    Revert "solve merge"
    This reverts commit 605afa2fb1714404a9715812ba64b570fa26ecc7.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,10 @@
-import { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useFonts } from 'expo-font'
 
-import { Analytics } from '$src/core/services/analytics/Analytics'
-import { Tracking } from '$src/core/services/tracking/Tracking'
+import QuranScreen from '$src/modules/quran/screens/QuranScreen'
 import ThemeManager from '$src/core/themes/ThemeManager'
 
-import QuranScreen from '$src/modules/quran/screens/QuranScreen'
-
 export default function App() {
-  useEffect(() => {
-    Tracking.log('App mounted.')
-    Analytics.logScreenView('App Mounted')
-  }, [])
-
   const [loaded] = useFonts({
     Uthmanic: require('./assets/fonts/Uthmanic.ttf'),
     SuraNames: require('./assets/fonts/SuraNames.ttf'),
