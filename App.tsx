3295721e137fba490e2ae5e7f4d3a3026302ff9d
SherifMega <shriefmarcato@gmail.com>
2023-06-14 19:24:45 +0300

    revert firebase changes + add sentry instead of firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useFonts } from 'expo-font'
 
-import QuranScreen from '$src/modules/quran/screens/QuranScreen'
+import { Monitoring } from '$src/core/services/monitoring/Monitoring'
 import ThemeManager from '$src/core/themes/ThemeManager'
 
+import QuranScreen from '$src/modules/quran/screens/QuranScreen'
+
 export default function App() {
+  useEffect(() => {
+    Monitoring.log({ message: 'App is Mounted!', category: 'Startup' })
+  }, [])
+
   const [loaded] = useFonts({
     Uthmanic: require('./assets/fonts/Uthmanic.ttf'),
     SuraNames: require('./assets/fonts/SuraNames.ttf'),
